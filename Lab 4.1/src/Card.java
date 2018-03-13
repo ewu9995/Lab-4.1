8f3d2bc22aba4e42b99474903ef226ced0d546d4
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-03-13 09:21:47 -0400

    commit

--- Lab 4.1/src/Card.java
+++ Lab 4.1/src/Card.java
@@ -1,4 +1,35 @@
 
 public class Card {
+  
+	String rank;
+	String suit;
+	int pointValue;
+	
+	public Card(String rank, String suit, int pointValue) {
+		
+	}
 
+	public String getRank() {
+		return rank;
+	}
+
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+
+	public String getSuit() {
+		return suit;
+	}
+
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
+	public int getPointValue() {
+		return pointValue;
+	}
+
+	public void setPointValue(int pointValue) {
+		this.pointValue = pointValue;
+	}
 }
--- Lab 4.1/src/Deck.java
+++ Lab 4.1/src/Deck.java
@@ -4,7 +4,7 @@ public class Deck {
 	public Deck(String[] ranks, String[] suits, int[] value) {
 		for(int i=0; i<ranks.length; i++) {
 			for(int j =0; j <suits.length;j++ ) {
-				unDealt.add(new Card(ranks[i], suits[j], value[i]))
+				unDealt.add(new Card(ranks[i], suits[j], value[i]));
 			}
 		}
 	}
