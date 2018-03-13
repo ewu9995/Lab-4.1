28f5485ebed2efb5ca221ed93f0f58c5c9666c88
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-03-13 09:17:10 -0400

    commit

---
+++ Lab 4.1/src/Deck.java
@@ -0,0 +1,11 @@
+
+public class Deck {
+ 
+	public Deck(String[] ranks, String[] suits, int[] value) {
+		for(int i=0; i<ranks.length; i++) {
+			for(int j =0; j <suits.length;j++ ) {
+				unDealt.add(new Card(ranks[i], suits[j], value[i]))
+			}
+		}
+	}
+}
